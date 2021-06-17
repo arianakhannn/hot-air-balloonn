7889a75fd9da563cb70884173922992e6f79a896
arianakhannn <81831308+arianakhannn@users.noreply.github.com>
2021-06-17 11:34:09 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,87 @@
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var db;
+var ref1;
+var pos;
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+
+
+
+
+  }
+
+//Function to set initial environment
+function setup() {
+  //database=firebase.database();
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+  db=firebase.database()
+  ref1=db.ref("balloon/position")
+  ref1.on("value",read)
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in left direction
+    write(-1,0);
+  }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in right direction
+    write(1,0);
+  }
+  else if(keyDown(UP_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in up direction
+    write(0,-1);
+  }
+  else if(keyDown(DOWN_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    //write code to move air balloon in down direction
+    write(0,+1);
+  }
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+
+
+function write(x,y){
+  db.ref('balloon/position').set({
+      x:balloon.x+x,
+      y:balloon.y+y
+  })
+  }
+
+function read(data){
+pos=data.val()
+console.log(pos)
+balloon.x=pos.x
+balloon.y=pos.y
+}
+
+
+
+
+
+
+
+
+
+
